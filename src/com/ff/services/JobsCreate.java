2c42b4fc49d54643699810a691e6fec647bfe216
FAYEL Florian <ffayel@gmail.com>
2014-06-23 11:55:54 +0200

    Petite modification pour tester l'envoie sur GITHUB

--- src/com/ff/services/JobsCreate.java
+++ src/com/ff/services/JobsCreate.java
@@ -4,6 +4,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 @Path("/create")
 public class JobsCreate {
@@ -11,5 +13,9 @@ public class JobsCreate {
 	@Produces(MediaType.TEXT_PLAIN)
 	public String create() {
 		return "Hello POST Jersey";
+	}
+	
+	public Response test(){
+		return Response.status(Status.ACCEPTED).build();
 	}
 }
